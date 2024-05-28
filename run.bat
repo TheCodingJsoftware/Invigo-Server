d8687b8dbb52c4e18d9cb645fb40f395cf6b5687
JareBear <jaredgrozz@gmail.com>
2024-05-28 13:39:33 -0500

    run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d C:\Users\Invigo\Inventory-Manager\server
+cd /d C:\Users\Invigo\Desktop\Invigo-Server
 C:\Users\Invigo\AppData\Local\Programs\Python\Python310\python.exe server.py %*
