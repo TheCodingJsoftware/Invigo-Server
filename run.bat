d3943751ee4d1eadf27ff3bef2d4f7714585e4f0
JareBear <jaredgrozz@gmail.com>
2024-08-12 15:00:45 -0500

    Updated to python 3.12

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d C:\Users\Invigo\Desktop\Invigo-Server
-C:\Users\Invigo\AppData\Local\Programs\Python\Python310\python.exe server.py %*
+C:\Users\Invigo\AppData\Local\Programs\Python\Python312\python.exe server.py %*
