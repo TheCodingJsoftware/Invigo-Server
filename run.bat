31d4a14b734ef620fe99d2138490699917a07b8c
JareBear <jaredgrozz@gmail.com>
2024-08-15 18:13:15 -0500

    Updated to use venv and new build system

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d C:\Users\Invigo\Desktop\Invigo-Server
-C:\Users\Invigo\AppData\Local\Programs\Python\Python312\python.exe server.py %*
+call venv\Scripts\activate.bat && python server.py %* %*
