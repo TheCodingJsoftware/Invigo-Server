43f415d950674142e97ec1a3d379796cdfff4df6
JareBear <jaredgrozz@gmail.com>
2024-08-15 18:26:50 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Users\Invigo\Desktop\Invigo-Server"
 git pull
-C:\Users\Invigo\AppData\Local\Programs\Python\Python312\python.exe setup.py
+python setup.py
 start cmd /k "call venv\Scripts\activate.bat && python server.py %*"
 exit
