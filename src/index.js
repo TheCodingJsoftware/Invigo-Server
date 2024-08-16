99b8427942ce9a88259d4f225392c5f37675e9dd
JareBear <jaredgrozz@gmail.com>
2024-08-16 12:54:46 -0500

    Changed homepage icons and names

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "beercss"
+
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach((el, index) => {
+        setTimeout(() => {
+            el.style.opacity = 1;
+            el.style.transition = 'opacity 1s';
+        }, index * 200);
+    });
+});
