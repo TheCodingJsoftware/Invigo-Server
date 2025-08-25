a5f640669295173288e6f5351683592c91e96e1c
Jared Gross <jaredgrozz@gmail.com>
2025-08-25 14:56:09 -0500

    Sorted vendors

--- handlers/purchase_orders/purchase_order_printouts.py
+++ handlers/purchase_orders/purchase_order_printouts.py
@@ -36,6 +36,7 @@ class PurchaseOrdersPageHandler(BaseHandler):
         all_purchase_orders = await self.purchase_orders_db.get_all_purchase_orders(include_data=True)
         all_purchase_orders = sorted(all_purchase_orders, key=lambda po: po["purchase_order_data"]["meta_data"]["purchase_order_number"], reverse=True)
         all_vendors = await self.vendors_db.get_all_vendors()
+        all_vendors = sorted(all_vendors, key=lambda v: v["name"].lower())
         shipping_method_lookup = {method.value: method.name.replace("_", " ").title() for method in ShippingMethod}
         self.render_template(
             "purchase_order_printouts.html",
