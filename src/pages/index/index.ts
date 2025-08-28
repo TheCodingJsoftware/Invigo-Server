4a7f40a4468e5f63f0edcce59f41eb6955d7ba98
Jared Gross <jaredgrozz@gmail.com>
2025-08-27 21:58:30 -0500

    Refactor theme button initialization to use AppearanceDialog for improved theme management

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,11 +1,15 @@
 import "beercss"
 import "@utils/theme"
+import {AppearanceDialog} from "@components/common/dialog/appearance-dialog";
 
 //  if ('serviceWorker' in navigator) {
 //      navigator.serviceWorker.register('/service-worker.js');
 //  }
 
 document.addEventListener("DOMContentLoaded", async () => {
+    const themeButtnon = document.getElementById("theme-button") as HTMLButtonElement;
+    themeButtnon.onclick = () => {new AppearanceDialog()};
+
     await fetch("/api/protected", {credentials: "include"}).then(async (res) => {
         if (res.ok) {
             const user = await res.json();
