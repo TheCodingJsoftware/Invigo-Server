fcc74d1932d48e3eccb204b07d58415d52605f0a
Jared Gross <jaredgrozz@gmail.com>
2025-08-27 21:59:39 -0500

    Add PartButton component for interactive part selection with tooltip and dialog support

---
+++ src/components/common/buttons/part-button.ts
@@ -0,0 +1,50 @@
+import {PartData} from "@components/workspace/parts/part-container";
+import {invertImages} from "@utils/theme";
+import {DialogComponent} from "@components/common/dialog/dialog-component";
+
+
+export class PartButton {
+    readonly element: HTMLButtonElement;
+    readonly part: PartData;
+
+    private readonly image: HTMLImageElement;
+    private readonly span: HTMLSpanElement;
+    private readonly tooltip: HTMLDivElement;
+
+    constructor(part: PartData) {
+        this.part = part;
+
+        this.element = document.createElement("button");
+        this.element.className = "part-button extra border small-round blur"
+        this.element.addEventListener("click", () => this.buttonPressed());
+
+        this.tooltip = document.createElement("div");
+        this.tooltip.className = "tooltip bottom";
+        this.tooltip.innerHTML = `
+            ${this.part.meta_data.gauge} ${this.part.meta_data.material}
+        `.trim();
+
+        this.image = document.createElement("img");
+        this.image.className = "responsive";
+        this.image.loading = "lazy";
+        this.image.src = `/images/${this.part.name}`
+        this.image.alt = this.part.name;
+
+        this.span = document.createElement("span");
+        this.span.textContent = this.part.name;
+
+        this.element.appendChild(this.image);
+        this.element.appendChild(this.span);
+        this.element.appendChild(this.tooltip);
+    }
+
+    buttonPressed() {
+        // const fileViewerDialog = new FileViewerDialog([this.part], this.filePath);
+
+        new DialogComponent({
+            title: this.part.name,
+            bodyContent: `<img class="responsive small-round" src="${this.image.src}" alt="${this.image.alt}" />`,
+        })
+        invertImages();
+    }
+}
