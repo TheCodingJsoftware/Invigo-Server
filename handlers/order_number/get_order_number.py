2ecc856bd06644b5962980232a84f81c45e0d6b4
Jared Gross <jaredgrozz@gmail.com>
2026-01-23 14:32:19 -0600

    Fix order number cache bug

--- handlers/order_number/get_order_number.py
+++ handlers/order_number/get_order_number.py
@@ -4,6 +4,12 @@ from handlers.base import BaseHandler
 
 
 class GetOrderNumberHandler(BaseHandler):
+    def set_default_headers(self):
+        # Prevent any client/proxy/CDN caching
+        self.set_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.set_header("Pragma", "no-cache")  # legacy HTTP/1.0
+        self.set_header("Expires", "0")
+
     async def get(self):
         jobs_info = await self.jobs_db.get_all_jobs()
 
